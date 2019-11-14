9de84a16381863970fb972c0e258dd161c69944f
Luis Alfredo Perez Linares <lalinares.1998@gmail.com>
2019-11-14 16:46:16 -0500

    Intento fallido correccion req.body empty

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.static('admin'))
 
 app.get('/', (req, res) => res.redirect('/login'))
-app.get('/login', (req, res) => res.sendFile(__dirname+'/admin/pages/examples/login.html'))
+app.get('/login', (req, res) => res.sendFile(__dirname+'/pages/examples/login.html'))
 app.post('/login', (req, res, next)=>{ 
-    console.log(req.body)
-    console.log(req.params)
+    console.log(req)
     res.redirect('/home')
 })
-app.get('/home',(req, res) => res.sendFile(__dirname+'/admin/index2.html'))
+app.get('/home',(req, res) => res.sendFile(__dirname+'/pages/index2.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
